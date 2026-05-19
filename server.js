b431dfe0d0b967ff2247bb901d12bbd137254a5e
Tassos Venetikidis <tassos.venetikidis@gmail.com>
2026-05-19 16:40:41 +0300

    Modified the server.js file to import the ideaRouter and implement its routes in the app

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
+import ideaRouter from "./routes/ideaRoutes.js";
 
 dotenv.config();
 
@@ -8,6 +9,9 @@ const app = express();
 const PORT = process.env.PORT || 8000;
 
 app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use("/api/ideas", ideaRouter);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
