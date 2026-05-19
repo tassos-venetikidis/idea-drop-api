064f9933bb650a0e626f44cd223974126015af51
Tassos Venetikidis <tassos.venetikidis@gmail.com>
2026-05-19 17:00:48 +0300

    Implemented the 404 fall-back route and the usage of the custom error handler

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import ideaRouter from "./routes/ideaRoutes.js";
+import errorHandler from "./middleware/errorHandler.js";
 
 dotenv.config();
 
@@ -11,7 +12,19 @@ const PORT = process.env.PORT || 8000;
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+// Routes
 app.use("/api/ideas", ideaRouter);
+
+// 404 Fall-back
+app.use((req, res, next) => {
+  const error = new Error(`Not Found - ${req.originalUrl}`);
+  res.status(404);
+  next(error);
+});
+
+// Error Handler
+app.use(errorHandler);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
