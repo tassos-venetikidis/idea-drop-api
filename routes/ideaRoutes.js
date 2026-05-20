230846590a32c2050993db29bd59bdcaaa3f2e6a
Tassos Venetikidis <tassos.venetikidis@gmail.com>
2026-05-20 17:42:26 +0300

    Implemented the delete and update routes

--- routes/ideaRoutes.js
+++ routes/ideaRoutes.js
@@ -47,6 +47,7 @@ router.get("/:ideaId", async (req, res, next) => {
 router.post("/", async (req, res, next) => {
   try {
     const { title, summary, description, tags } = req.body;
+
     if (!title?.trim() || !summary?.trim() || !description?.trim()) {
       res.status(400);
       throw new Error("Title, summary and description are required!");
@@ -71,6 +72,75 @@ router.post("/", async (req, res, next) => {
     res.status(201).json(savedIdea);
   } catch (err) {
     console.log(err.message);
+    next(err);
+  }
+});
+
+// @route        PUT /api/ideas/:ideaId
+// @description  Update single idea
+// @access       Public
+router.put("/:ideaId", async (req, res, next) => {
+  const { ideaId } = req.params;
+
+  try {
+    const { title, summary, description, tags } = req.body;
+
+    if (!title?.trim() || !summary?.trim() || !description?.trim()) {
+      res.status(400);
+      throw new Error("Title, summary and description are required!");
+    }
+
+    const updatedIdea = await Idea.findByIdAndUpdate(
+      ideaId,
+      {
+        title: title.trim(),
+        summary: summary.trim(),
+        description: description.trim(),
+        tags:
+          typeof tags === "string"
+            ? tags
+                .split(",")
+                .map((tag) => tag.trim())
+                .filter((tag) => tag !== "")
+            : Array.isArray(tags)
+              ? tags
+              : [],
+      },
+      { new: true, runValidators: true },
+    );
+
+    if (!updatedIdea) {
+      res.status(404);
+      throw new Error("Idea Not Found");
+    }
+
+    res.json(updatedIdea);
+  } catch (err) {
+    console.log(err.message);
+    next(err);
+  }
+});
+
+// @route        DELETE /api/ideas/:ideaId
+// @description  Delete single idea
+// @access       Public
+router.delete("/:ideaId", async (req, res, next) => {
+  const { ideaId } = req.params;
+
+  if (!mongoose.Types.ObjectId.isValid(ideaId)) {
+    res.status(404);
+    throw new Error("Idea Not Found");
+  }
+
+  try {
+    const idea = await Idea.findByIdAndDelete(ideaId);
+    if (!idea) {
+      res.status(404);
+      throw new Error("Idea Not Found");
+    }
+    res.json({ message: "Idea deleted succesfully." });
+  } catch (err) {
+    console.log(err);
     next(err);
   }
 });
