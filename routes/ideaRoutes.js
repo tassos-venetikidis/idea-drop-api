139015bf5af53c0d584c93c59667155c97ae88f0
Tassos Venetikidis <tassos.venetikidis@gmail.com>
2026-05-20 16:19:30 +0300

    Implemented the get all ideas and get single idea routes

--- routes/ideaRoutes.js
+++ routes/ideaRoutes.js
@@ -1,18 +1,44 @@
 import express from "express";
+import Idea from "../models/Idea.js";
+import mongoose from "mongoose";
 
 const router = express.Router();
 
 // @route        GET /api/ideas
 // @description  Get all ideas
 // @access       Public
-router.get("/", (req, res) => {
-  const ideas = [
-    { id: 1, title: "Idea 1", description: "This is Idea 1" },
-    { id: 2, title: "Idea 2", description: "This is Idea 2" },
-    { id: 3, title: "Idea 3", description: "This is Idea 3" },
-  ];
+router.get("/", async (req, res, next) => {
+  try {
+    const ideas = await Idea.find();
+    res.json(ideas);
+  } catch (err) {
+    console.log(err);
+    next(err);
+  }
+});
 
-  res.json(ideas);
+// @route        GET /api/ideas/:ideaId
+// @description  Get single idea
+// @access       Public
+router.get("/:ideaId", async (req, res, next) => {
+  const { ideaId } = req.params;
+
+  if (!mongoose.Types.ObjectId.isValid(ideaId)) {
+    res.status(404);
+    throw new Error("Idea Not Found");
+  }
+
+  try {
+    const idea = await Idea.findById(ideaId);
+    if (!idea) {
+      res.status(404);
+      throw new Error("Idea Not Found");
+    }
+    res.json(idea);
+  } catch (err) {
+    console.log(err);
+    next(err);
+  }
 });
 
 // @route        POST /api/ideas
