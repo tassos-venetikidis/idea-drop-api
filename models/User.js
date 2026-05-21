67e32d072c63850516854faf0ed3d231496c4c0b
Tassos Venetikidis <tassos.venetikidis@gmail.com>
2026-05-21 17:00:29 +0300

    Added a matchPassword method in the user schema

--- models/User.js
+++ models/User.js
@@ -34,6 +34,11 @@ userSchema.pre("save", async function () {
   this.password = await bcrypt.hash(this.password, salt);
 });
 
+// Compare entered password with hashed password
+userSchema.methods.matchPassword = async function (enteredPassword) {
+  return await bcrypt.compare(enteredPassword, this.password);
+};
+
 const User = mongoose.model("User", userSchema);
 
 export default User;
