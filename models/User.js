b64306ab4688e64622cabbfe36eaef7233bd270a
Tassos Venetikidis <tassos.venetikidis@gmail.com>
2026-05-21 15:49:16 +0300

    Fixed two small bugs with the code

--- models/User.js
+++ models/User.js
@@ -1,6 +1,5 @@
 import mongoose from "mongoose";
 import bcrypt from "bcryptjs";
-import { use } from "react";
 
 const userSchema = new mongoose.Schema(
   {
@@ -22,16 +21,17 @@ const userSchema = new mongoose.Schema(
       minlength: 6,
     },
   },
-  { timestamps: true },
+  {
+    timestamps: true,
+  },
 );
 
 // Hash passwords before saving
-userSchema.pre("save", async function (next) {
+userSchema.pre("save", async function () {
   if (!this.isModified("password")) return next();
 
   const salt = await bcrypt.genSalt(10);
   this.password = await bcrypt.hash(this.password, salt);
-  next();
 });
 
 const User = mongoose.model("User", userSchema);
