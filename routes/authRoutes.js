cffe1f79e40c1c5d99a9f56f43fff60995748177
Tassos Venetikidis <tassos.venetikidis@gmail.com>
2026-05-21 16:26:52 +0300

    Implemented the creation and response with access and refresh tokens in the register new user route

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -2,6 +2,7 @@ import express from "express";
 import mongoose from "mongoose";
 import User from "../models/User.js";
 import Idea from "../models/Idea.js";
+import { generateToken } from "../utils/generateToken.js";
 
 const router = express.Router();
 
@@ -25,7 +26,21 @@ router.post("/register", async function (req, res, next) {
 
     const user = await User.create({ name, email, password });
 
+    // Create Tokens
+    const payload = { userId: user._id.toString() };
+    const accessToken = await generateToken(payload, "1m");
+    const refreshToken = await generateToken(payload, "30d");
+
+    // Set refresh token in a HTTP-Only cookie
+    res.cookie("refreshToken", refreshToken, {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === "production",
+      sameSite: "none",
+      maxAge: 30 * 24 * 60 * 60 * 1000, // 30 days expressed in msec
+    });
+
     res.status(201).json({
+      accessToken,
       id: user._id,
       name: user.name,
       email: user.email,
