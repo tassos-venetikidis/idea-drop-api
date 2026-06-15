c8a67c4761965781e415a0e41d7ec3ff1675a1cc
Tassos Venetikidis <tassos.venetikidis@gmail.com>
2026-06-15 23:23:38 +0300

    Implemented a different approach to using the cors middleware

--- server.js
+++ server.js
@@ -23,12 +23,27 @@ const allowedOrigins = [
   "https://idea-drop-fbfwhui8t-tassos-venetikidis-projects.vercel.app/",
 ];
 
-app.use(
-  cors({
-    origin: allowedOrigins,
-    credentials: true,
-  }),
-);
+const corsOptions = {
+  origin: function (origin, callback) {
+    // allow requests with no origin (like mobile apps or curl requests)
+    if (!origin) return callback(null, true);
+
+    if (allowedOrigins.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+  credentials: true,
+};
+
+app.use(cors(corsOptions));
+
+// app.use(
+//   cors({
+//     origin: allowedOrigins,
+//   }),
+// );
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
